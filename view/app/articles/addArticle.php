da8cb06eddb8c402a4aaf99bb82eb66983926bcf
Ophelie <freville.ophelie@neuf.fr>
2023-12-15 11:48:15 +0100

    Ajout d'un formulaire

--- view/app/articles/addArticle.php
+++ view/app/articles/addArticle.php
@@ -1,2 +1,14 @@
-<?php
-$this->dd($formAdd);
+<section>
+    <h3>Ajout d'un article</h3>
+    <form method="POST">
+        <?= $formAdd->label('Titre') ?>
+        <?= $formAdd->input('Titre') ?>
+        <?= $formAdd->error('Titre') ?>
+
+        <?= $formAdd->label('Contenu') ?>
+        <?= $formAdd->textarea('Contenu') ?>
+        <?= $formAdd->error('Contenu') ?>
+
+        <?= $formAdd->submit('', 'Ajouter mon article') ?>
+    </form>
+</section>
