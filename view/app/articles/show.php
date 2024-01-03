2ee536771a58dbb6901441175651a53c828c4d32
Ophelie <freville.ophelie@neuf.fr>
2024-01-03 10:59:04 +0100

    FInal ? modif

--- view/app/articles/show.php
+++ view/app/articles/show.php
@@ -1,18 +1,26 @@
 <section>
+
     <h3><?= $note->title ?></h3>
-    <p>Auteur : <?= $user->findById($note->author, 'id')->firstname . ' ' .
-                    $user->findById($note->author, 'id')->lastname ?></p>
+
+    <p>Auteur : <?= $user->findById($note->author, 'id')->firstname . ' ' .$user->findById($note->author, 'id')->lastname ?></p>
+
     <p><?= $note->content ?></p>
+
     <p>Publié le : <?= $note->createdAt ?></p>
+
     <p>Modifié le : <?= $note->modifiedAt ?></p>
+
     <p>
         <a href="<?= $view->path('edit', [$note->id]) ?>">
             Modifier
         </a>
     </p>
+
     <p>
-        <a href="<?= $view->path('delete', [$note->id]) ?>">
+        <a href="<?= $view->path('delete', [$note->id]) ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer DEFINITIVEMENT cet article ?')">
             Supprimer
         </a>
+
     </p>
+    
 </section>
